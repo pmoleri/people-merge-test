a5273abf7f85dd38f52ec03db6738697cbbae496
pmoleri <pmoleri@gmail.com>
2022-01-25 18:15:12 -0300

    People app merge test initial version

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using IgniteUI.Blazor.Controls;
+using PeopleAppMergeTest;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+RegisterIgniteUI(builder.Services);
+
+await builder.Build().RunAsync();
+
+void RegisterIgniteUI(IServiceCollection services)
+{
+    services.AddIgniteUIBlazor(typeof(IgbButtonModule));
+    services.AddIgniteUIBlazor(typeof(IgbRippleModule));
+    services.AddIgniteUIBlazor(typeof(IgbIconModule));
+    services.AddIgniteUIBlazor(typeof(IgbAvatarModule));
+    services.AddIgniteUIBlazor(typeof(IgbListModule));
+    services.AddIgniteUIBlazor(typeof(IgbInputModule));
+    services.AddIgniteUIBlazor(typeof(IgbDatePickerModule));
+}
